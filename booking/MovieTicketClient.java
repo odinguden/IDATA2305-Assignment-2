e6c29f27bc78e4241290c42fe80834e31ac59988
Odin Lyngsgård <odin24@outlook.com>
2024-03-04 10:04:28 +0100

    Assignment skeleton

---
+++ booking/MovieTicketClient.java
@@ -0,0 +1,15 @@
+package booking;
+
+public class MovieTicketClient extends Thread {
+
+
+	public MovieTicketClient() {
+		// TBD
+	}
+	
+	@Override
+	public void run() {
+		// TBD
+	}
+
+}
---
+++ booking/MovieTicketServer.java
@@ -0,0 +1,19 @@
+package booking;
+
+public class MovieTicketServer {
+
+	public MovieTicketServer(int numberOfSeats, String movieName) {
+		// TBD
+	}
+
+	public void bookTicket() {
+		// TBD
+
+		if ((availableSeats - numberOfSeats) < 0) {
+			// TBD
+		}
+
+		// TBD
+	}
+
+}
