4a8fd4e4f3860eb07132e8de7646e254cd4ab606
Odin Lyngsgård <odin24@outlook.com>
2024-03-04 10:13:28 +0100

    Movie ticket server implemented

--- booking/MovieTicketServer.java
+++ booking/MovieTicketServer.java
@@ -1,19 +1,24 @@
 package booking;
 
 public class MovieTicketServer {
+	private String movieName;
+	private int availableSeats;
 
-	public MovieTicketServer(int numberOfSeats, String movieName) {
-		// TBD
+	public MovieTicketServer(int seats, String movieName) {
+		this.movieName = movieName;
+		this.availableSeats = seats;
 	}
 
-	public void bookTicket() {
-		// TBD
+	public void bookTicket(String userName, int numberOfSeats) {
+		System.out.println("Hi," + userName + " : " + availableSeats + " : Seats available for " + movieName);
 
 		if ((availableSeats - numberOfSeats) < 0) {
-			// TBD
+			System.out.println("Hi," + userName + " : Seats not available for " + movieName);
+			return;
 		}
-
-		// TBD
+		availableSeats = availableSeats - numberOfSeats;
+		System.out.println("Hi," + userName + " : " + numberOfSeats + " Seats booked successfully for" + movieName);
+		System.out.println(availableSeats + " remaining for " + movieName);
 	}
 
 }
